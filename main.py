36befb647c896856f574c938dd81727155e6ea4e
Konrad Kosuge <glenbrooktechservice@gmail.com>
2023-02-05 19:57:22 +0100

    webdriver sorted out, updated webdriver passed path as service object

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 
 #import statements
 import requests
-import selenium
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import time
+'''
 #go to 1668 website
 homeurl = 'https://www.1688.com/'
 homepage= requests.get(url=homeurl)
 homeresult = BeautifulSoup(homepage.content, 'html.parser')
-print(homeresult)
+print(homeresult)'''
+
+#selenium webdriver using
+
+pathtowebdriver= Service('C:\\Users\\konch\\OneDrive\\Desktop\\chromedriver.exe')
+driver = webdriver.Chrome(service=pathtowebdriver)
+driver.get("https://google.com")
+time.sleep(1000)
 
 
 
