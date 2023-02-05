0f44ad5104000348d0daaea1549eb92e1b22ab9d
Konrad Kosuge <glenbrooktechservice@gmail.com>
2023-02-05 19:34:59 +0100

    import and requests

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 
+#import statements
+import requests
+import selenium
+from bs4 import BeautifulSoup
+#go to 1668 website
+homeurl = 'https://www.1688.com/'
+homepage= requests.get(url=homeurl)
+homeresult = BeautifulSoup(homepage.content, 'html.parser')
+print(homeresult)
+
+
+
+
+#navigate to products and get information
+
+#write into excel
+
+#add appropiate code to product
+
